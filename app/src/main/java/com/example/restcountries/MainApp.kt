38a47619290dd307baa5d01ca20329ea256504b4
Darush <darushfaqiri@gmail.com>
2023-04-09 01:41:45 +0300

    build: integrate Timber logging library

--- app/src/main/java/com/example/restcountries/MainApp.kt
+++ app/src/main/java/com/example/restcountries/MainApp.kt
@@ -2,6 +2,16 @@ package com.example.restcountries
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class MainApp: Application()
+class MainApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG)
+            Timber.plant(Timber.DebugTree())
+    }
+
+}
