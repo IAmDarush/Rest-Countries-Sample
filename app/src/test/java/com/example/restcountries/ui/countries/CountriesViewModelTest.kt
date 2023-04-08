9bedeabba585949324eacf01d9e95f054b41b1c8
Darush <darushfaqiri@gmail.com>
2023-04-08 22:32:54 +0300

    test: spec out the countries list view model behaviour

---
+++ app/src/test/java/com/example/restcountries/ui/countries/CountriesViewModelTest.kt
@@ -0,0 +1,42 @@
+package com.example.restcountries.ui.countries
+
+import org.junit.Test
+
+class CountriesViewModelTest {
+
+
+    @Test
+    fun `Given the screen is opened, Then start fetching the countries list`() {
+
+    }
+
+    @Test
+    fun `Given the countries list is being fetched, When it it succeeds, Then populate the list`() {
+
+    }
+
+    @Test
+    fun `Given the countries list is being fetched, When it fails, Then display an error`() {
+
+    }
+
+    @Test
+    fun `Given the countries list is fetched, When the user wants to search for a specific country, Then search for the country`() {
+
+    }
+
+    @Test
+    fun `Given a specific country is being searched, When the result is found, Then reduce the list`() {
+
+    }
+
+    @Test
+    fun `Given a specific country is being searched, When the result is not found, Then show empty list`() {
+
+    }
+    @Test
+    fun `Given a specific country is being searched, When the search fails, Then show error message`() {
+
+    }
+
+}
