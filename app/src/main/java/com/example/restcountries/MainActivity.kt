ec7dad17a61e5a7cb81c5e06ee51b1a69e905a28
Darush <darushfaqiri@gmail.com>
2023-04-09 01:38:44 +0300

    build: integrate Hilt dependency injection framework

--- app/src/main/java/com/example/restcountries/MainActivity.kt
+++ app/src/main/java/com/example/restcountries/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.restcountries.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
---
+++ app/src/main/java/com/example/restcountries/MainApp.kt
@@ -0,0 +1,7 @@
+package com.example.restcountries
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApp: Application()
