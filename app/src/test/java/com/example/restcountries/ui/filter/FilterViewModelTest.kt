025328e0e9fb748be975d19106149ed7dbd5772a
Darush <darushfaqiri@gmail.com>
2023-06-02 19:36:38 +0300

    test: spec out the FilterViewModel tests

---
+++ app/src/test/java/com/example/restcountries/ui/filter/FilterViewModelTest.kt
@@ -0,0 +1,58 @@
+package com.example.restcountries.ui.filter
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+class FilterViewModelTest {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `Given the filter screen is opened, When the user wants to sort alphabetically, Then check the alphabetical sort order`() =
+        runTest {
+
+        }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `Given the filter screen is opened, When the user wants deselect the sort type, Then select none of the sort types`() =
+        runTest {
+
+        }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `Given the filter screen is opened, When the user wants to filter by a particular subregion, Then check all the selected subregions`() =
+        runTest {
+
+        }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `Given the filter screen is opened, When the user wants deselect all the subregions, Then deselect the subregions`() =
+        runTest {
+
+        }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `Given the filter screen is opened, When the user wants to clear all the filtering, Then clear all the filters`() =
+        runTest {
+
+        }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `Given the filter screen is opened, When the user wants to apply all the filtering, Then apply the filters`() =
+        runTest {
+
+        }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `Given the filter screen is opened, When there is a saved instance state, Then update the ui state accordingly`() =
+        runTest {
+
+        }
+
+}
